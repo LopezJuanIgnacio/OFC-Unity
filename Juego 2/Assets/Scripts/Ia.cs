79584f36cfa00f32cb0a81f09f318dca8051b456
Juan ignacio Lopez <94252732+LopezElFacha@users.noreply.github.com>
2022-06-29 17:18:46 -0300

    aguante boke

---
+++ Juego 2/Assets/Scripts/Ia.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ia : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public LayerMask piso;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit info;
+
+            if(Physics.Raycast(rayo, out info, 100f, piso)){
+                agent.SetDestination(info.point);
+            }
+        }
+    }
+}
